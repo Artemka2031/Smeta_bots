8cd3a1d632e437834a0982a50d0e06bf2be1ffeb
Artemka2031 <87371014+Artemka2031@users.noreply.github.com>
2024-06-30 15:15:46 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-projects = [
-    # {"token": "6780094138:AAGqgwm5-RNuXOWGfvtGGDCXWaYsQbJWLns",
-    {"token": "6397282799:AAG-kCn6jHua9W9y48L20K_IDM-5kPEY3P0",
-     "url": "https://docs.google.com/spreadsheets/d/1pQETvVwO1EtDlbz6zGxfF0ndiS4xGZfJyJkJfSvHPpM",
-     "db": "Data/project1.db"}
-]
