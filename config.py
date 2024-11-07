ee064acb65be5f49020f01d845e014bcf874046d
root <root@dkuskblxny.local>
2024-11-07 12:52:43 +0000

    Server usage

--- config.py
+++ config.py
@@ -1,62 +1,98 @@
 projects = [
-    # {
-    #     "name": "Дом в коттеджном посёлке Крокусы",
-    #     "token": "6780094138:AAGqgwm5-RNuXOWGfvtGGDCXWaYsQbJWLns",
-    #     "url": "https://docs.google.com/spreadsheets/d/1pQETvVwO1EtDlbz6zGxfF0ndiS4xGZfJyJkJfSvHPpM",
-    #     "db": "project1_backup"
-    # },
-    # {
-    #     "name": "Ремонт трещен Новая 7",
-    #     "token": "6491942405:AAGyaRiYXJKgO0HBQOHUqvU1mraWU8bsE4E",
-    #     "url": "https://docs.google.com/spreadsheets/d/17HFAmK8fA3r3hU9bpL1hXGpLKwJm4IjE3at_oYHWA6s",
-    #     "db": "Novaya"
-    # },
-    # {
-    #     "name": "Лестница, плитка, инъекции Аполло",
-    #     "token": "6170818474:AAFMifYEtt4rR-45kprh-ffYEElEms1Yorw",
-    #     "url": "https://docs.google.com/spreadsheets/d/1hNSHKPCeZGIkdgywyG82wTWA-NyPX91Ee2ybMFq0EZo",
-    #     "db": "apollo"
-    # },
-    # {
-    #     "name": "Парамет Лаврики",
-    #     "token": "6977324764:AAHMJKTce51cz8CWhWByeC_Wom5RWIrJj4Y",
-    #     "url": "https://docs.google.com/spreadsheets/d/1S98s9mpi_DE9XxK6siw7vBeKNOWwXIxzy_-uv3geNxI",
-    #     "db": "lavriki"
-    # },
-    # {
-    #     "name": "Аварийные работы Школьная",
-    #     "token": "7411461624:AAGROwbw3w-Eh7nb1UcTRPyPGmABzB_AbXI",
-    #     "url": "https://docs.google.com/spreadsheets/d/1PrmpR380C_-OCwsrLRWanV_OmMglWxteY0nVykCX8qA",
-    #     "db": "Shkolnaya"
-    # },
-    # {
-    #     "name": "Янино 2-ая перекладка кирпича",
-    #     "token": "6031682099:AAE_32LYrCyRj0fwxMozqOX6ptHrHxp6ZPA",
-    #     "url": "https://docs.google.com/spreadsheets/d/1zCZ_aiNAbng1mhtOdBCxr8WfHC2EMFhd_ZhzgAdGtwI",
-    #     "db": "Yanino_second"
-    # },
-    # {
-    #     "name": "УК-36 Пулковское шоссе",
-    #     "token": "6910553886:AAGlEliEdjoFVuMnbXPq8HG34jgTG1bA3KM",
-    #     "url": "https://docs.google.com/spreadsheets/d/1h6810dwctl2TbeFRVLxWoyKusUeZZmpfJXJ8wBTD2V0",
-    #     "db": "Pulkovskoe_shosse"
-    # },
-    # {
-    #     "name": "Кушелевка - пандус",
-    #     "token": "6337357563:AAH3H5TIW-QLvLXd-WVBAiIplZPZJFTNUfU",
-    #     "url": "https://docs.google.com/spreadsheets/d/1aQhH_vUaCNksxR5JNRxFcaK-ad7zffd2kEvytG5uFis",
-    #     "db": "Kushelevka_pandus"
-    # },
-    # {
-    #     "name": "Острава Ветеранов",
-    #     "token": "7512576675:AAFmWQYjdeLINX3G1WTUCb6RHAIVO865UAc",
-    #     "url": "https://docs.google.com/spreadsheets/d/1t3LYyXpPApFXfpnEpl01E8lM2GGSbzXnKZU0J4RW1EU",
-    #     "db": "Ostrava_veteranov"
-    # },
     {
-        "name": "Тест нового функционала",
-        "token": "6397282799:AAG-kCn6jHua9W9y48L20K_IDM-5kPEY3P0",
-        "url": "https://docs.google.com/spreadsheets/d/1ATj6_9W7MeC0AyUtrgQ_ILD0WfF2LM3Nt6pJN09aDt0",
-        "db": "Test"
+        "name": "Дом в коттеджном посёлке Крокусы",
+        "token": "6780094138:AAGqgwm5-RNuXOWGfvtGGDCXWaYsQbJWLns",
+        "url": "https://docs.google.com/spreadsheets/d/1pQETvVwO1EtDlbz6zGxfF0ndiS4xGZfJyJkJfSvHPpM",
+        "db": "project1_backup"
+    },
+    {
+        "name": "Ремонт трещен Новая 7",
+        "token": "6491942405:AAGyaRiYXJKgO0HBQOHUqvU1mraWU8bsE4E",
+        "url": "https://docs.google.com/spreadsheets/d/17HFAmK8fA3r3hU9bpL1hXGpLKwJm4IjE3at_oYHWA6s",
+        "db": "Novaya"
+    },
+    {
+        "name": "Лестница, плитка, инъекции Аполло",
+        "token": "6170818474:AAFMifYEtt4rR-45kprh-ffYEElEms1Yorw",
+        "url": "https://docs.google.com/spreadsheets/d/1hNSHKPCeZGIkdgywyG82wTWA-NyPX91Ee2ybMFq0EZo",
+        "db": "apollo"
+    },
+    {
+        "name": "Парамет Лаврики",
+        "token": "6977324764:AAHMJKTce51cz8CWhWByeC_Wom5RWIrJj4Y",
+        "url": "https://docs.google.com/spreadsheets/d/1S98s9mpi_DE9XxK6siw7vBeKNOWwXIxzy_-uv3geNxI",
+        "db": "lavriki"
+    },
+    {
+        "name": "Аварийные работы Школьная",
+        "token": "7411461624:AAGROwbw3w-Eh7nb1UcTRPyPGmABzB_AbXI",
+        "url": "https://docs.google.com/spreadsheets/d/1PrmpR380C_-OCwsrLRWanV_OmMglWxteY0nVykCX8qA",
+        "db": "Shkolnaya"
+    },
+    {
+        "name": "Янино 2-ая перекладка кирпича",
+        "token": "6031682099:AAE_32LYrCyRj0fwxMozqOX6ptHrHxp6ZPA",
+        "url": "https://docs.google.com/spreadsheets/d/1zCZ_aiNAbng1mhtOdBCxr8WfHC2EMFhd_ZhzgAdGtwI",
+        "db": "Yanino_second"
+    },
+    {
+        "name": "УК-36 Пулковское шоссе",
+        "token": "6910553886:AAGlEliEdjoFVuMnbXPq8HG34jgTG1bA3KM",
+        "url": "https://docs.google.com/spreadsheets/d/1h6810dwctl2TbeFRVLxWoyKusUeZZmpfJXJ8wBTD2V0",
+        "db": "Pulkovskoe_shosse"
+    },
+    {
+        "name": "Кушелевка - пандус",
+        "token": "6337357563:AAH3H5TIW-QLvLXd-WVBAiIplZPZJFTNUfU",
+        "url": "https://docs.google.com/spreadsheets/d/1aQhH_vUaCNksxR5JNRxFcaK-ad7zffd2kEvytG5uFis",
+        "db": "Kushelevka_pandus"
+    },
+    {
+        "name": "Острава Ветеранов",
+        "token": "7512576675:AAFmWQYjdeLINX3G1WTUCb6RHAIVO865UAc",
+        "url": "https://docs.google.com/spreadsheets/d/1t3LYyXpPApFXfpnEpl01E8lM2GGSbzXnKZU0J4RW1EU",
+        "db": "Ostrava_veteranov"
+    },
+    {
+        "name": "Сотрудник Хамза",
+        "token": "7911467498:AAGS73VZA-aUBr1PfvXWl5qwvtN2_kBM7Qw",
+        "url": "https://docs.google.com/spreadsheets/d/1c0KHn7MtWzBPe9dlytsVJvHUq3E_WhdHc43-3aMJ6mc",
+        "db": "employee_hamza"
+    },
+    {
+        "name": "Сотрудник Равшан",
+        "token": "7226646575:AAEo9TkPhm5lypxRiczAQbA2f8_KVvXwEQ4",
+        "url": "https://docs.google.com/spreadsheets/d/104m2fWawo-KWKNGshilWmKHfdLr94q0AXiuK8dojeLc",
+        "db": "employee_ravshan"
+    },
+    {
+        "name": "Сотрудник Али",
+        "token": "7235513556:AAFTlXgGdnkWZVy6gHnbCOkkBYuZZlJp7gg",
+        "url": "https://docs.google.com/spreadsheets/d/1BSDCrtv3mBSjiWDwu5NfBxZ0TUIAeSfisa5ODXd2OZU",
+        "db": "employee_ali"
+    },
+    {
+        "name": "Сотрудник Азиз",
+        "token": "8136652422:AAEBZMPOZOHZCtkokb1Lk5g6ho8XNa2t2Bg",
+        "url": "https://docs.google.com/spreadsheets/d/1COVlte_OHRbrfeV8R3s42Iin6VA9NW6CT-GmNS-m4C0",
+        "db": "employee_aziz"
+    },
+    {
+        "name": "Сотрудник Коха",
+        "token": "8067874611:AAHHfFin8THRf1a7LrrhuyAXuwk8vTAa3Ho",
+        "url": "https://docs.google.com/spreadsheets/d/1DJlg3aaXm1nlu0wY1McvGT8ZQwd2tLrBYhjDId72ytw",
+        "db": "employee_koha"
+    },
+    {
+        "name": "Сотрудник Саид",
+        "token": "7549196638:AAHfbOZURjdF0fwSeDG4rrBm92SWHtdIi6g",
+        "url": "https://docs.google.com/spreadsheets/d/1Xbo4eGQAk6IE6O0r3UEc-u3nvwuPZwZXS02vnydAqsY",
+        "db": "employee_said"
+    },
+    {
+        "name": "Сотрудник Парвиз",
+        "token": "7504216697:AAFrTkE1tApcebDmNG5c6SRXH1zW0Gf6nUA",
+        "url": "https://docs.google.com/spreadsheets/d/1YTRG2dCYyWwDex6baMoZCIiTkXG7SW3oThi0M0wbvxI",
+        "db": "employee_parviz"
     }
 ]
